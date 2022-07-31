33d8eb50946fa25aeeef357c1cd9dac52d1daefc
developersview <pcslg1998@gmail.com>
2022-07-31 12:38:53 +0530

    initial commit

---
+++ src/test/java/com/developersview/SpringSecurityExample/SpringSecurityExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developersview.SpringSecurityExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
